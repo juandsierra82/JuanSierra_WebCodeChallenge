0c28af128fde45a7d33cef572168e977dc68ffcf
juandsierra82 <juandsierra82@gmail.com>
2015-11-09 11:16:49 -0600

    first yo build

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name juanSierraWebCodeChallengeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the juanSierraWebCodeChallengeApp
+ */
+angular.module('juanSierraWebCodeChallengeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
